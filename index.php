13037a8c64cf51b3ff6bdbd8cf9966d9eb3aad43
Chris Gauthier <cpgauthier@gmail.com>
2011-11-14 13:57:42 -0500

    Updated reference to toybox shared stuff.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Fissure :: Words Are Toys</title>
+		<title>Fissure</title>
 		<link rel="stylesheet" type="text/css" media="screen" href="fissure.css">
 		<link rel="stylesheet" type="text/css" media="screen" href="/shared/toybox.css">
 		<script type="text/javascript" src="/shared/jquery-1.6.2.js"></script>
